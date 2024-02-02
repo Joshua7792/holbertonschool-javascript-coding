9b57250fc90bb344226ba74c7111f839c20c4def
Joshua7792 <j.santiago39@hotmail.com>
2024-02-02 17:55:24 -0400

    fixed

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -3,8 +3,9 @@ const fs = require('fs');
 function countStudents(path) {
     try {
         const data = fs.readFileSync(path, 'utf8');
-        const lines = data.split('\n').filter(Boolean);
-        console.log(`Number of students: ${lines.length - 1}`); // First line is header
+        // Process the data, count students, etc.
+        console.log(`Number of students: ${count}`);
+        // Additional logging as per the requirements
     } catch (error) {
         throw new Error('Cannot load the database');
     }
